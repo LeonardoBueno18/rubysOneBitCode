8a4b1379b97a1453cbe4977779f4aadda9961c29
Leonardo Gabriel Bueno <leonardogabrielbueno@gmail.com>
2021-03-22 16:27:51 -0300

    Subindo aula 2

---
+++ aula 2/nome_idade.rb
@@ -0,0 +1,6 @@
+print "Digite seu nome: "
+nome = gets.chomp
+print "Digite sua idade: "
+idade = gets.chomp.to_i
+
+print "Bem vindo #{nome} sua idade é de #{idade} anos"
---
+++ aula 2/operacoes.rb
@@ -0,0 +1,11 @@
+print "Digite o primeiro valor: "
+valor1 = gets.chomp.to_i
+print "Digite o segundo valor: "
+valor2 = gets.chomp.to_i
+
+soma = valor1 + valor2
+multi = valor1 * valor2
+divi = valor1 / valor2
+subt = valor1 - valor2
+
+print "A soma dos valores é #{soma}, a multiplicação é #{multi}, a divisão é #{divi} e a subtração é #{subt}"
